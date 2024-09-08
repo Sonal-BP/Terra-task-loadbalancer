845e17961656105d2f128ce9845f17c83d31ac56
Sonal-BP <108402618+Sonal-BP@users.noreply.github.com>
2024-09-08 15:21:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_lb_target_group" "app_tg" {
   name     = "app-tg"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
+  vpc_id   = var.vpc_id
 
   health_check {
     path                = "/"
