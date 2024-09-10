85580ab9c6f04c8840e1f439003a163388bcec1e
Sonal-BP <108402618+Sonal-BP@users.noreply.github.com>
2024-09-10 13:33:29 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,14 +1,7 @@
 variable "vpc_id" {
-  description = "The ID of the VPC where the ALB and Target Group will be created"
-  type        = string
+  type = string
 }
 
-variable "subnet_ids" {
-  description = "List of subnet IDs where the ALB will be placed"
-  type        = list(string)
-}
-
-variable "security_group_ids" {
-  description = "List of security group IDs to attach to the ALB"
-  type        = list(string)
+variable "public_subnet_ids" {
+  type = list(string)
 }
