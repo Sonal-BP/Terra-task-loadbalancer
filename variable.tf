e813320f3825730459cd8fbb1d0f3828b5e9864a
Sonal-BP <108402618+Sonal-BP@users.noreply.github.com>
2024-09-10 14:27:16 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,6 +2,18 @@ variable "vpc_id" {
   type = string
 }
 
-variable "public_subnet_ids" {
+variable "private_subnet_ids" {
   type = list(string)
+}
+
+variable "target_group_arn" {
+  type = string
+}
+
+variable "ami_id" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
 }
