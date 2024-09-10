fde4597f4caee45b70348cd4d63834ac77f55a67
Sonal-BP <108402618+Sonal-BP@users.noreply.github.com>
2024-09-10 13:33:11 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,8 @@
+output "alb_target_group_arn" {
+  value = aws_lb_target_group.alb_tg_1.arn
+}
+
 output "alb_dns_name" {
-  description = "The DNS name of the Application Load Balancer"
-  value       = aws_lb.app_lb.dns_name
+  description = "DNS name of the ALB"
+  value       = aws_lb.alb_1.dns_name
 }
