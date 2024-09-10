1b8b23d5838024b39333bbb30f72a51f448f822c
Sonal-BP <108402618+Sonal-BP@users.noreply.github.com>
2024-09-10 16:46:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,3 @@
-resource "aws_security_group" "ec2_sg" {
-  vpc_id = var.vpc_id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-
-
 resource "aws_launch_configuration" "nginx-launch_config" {
   name          = "nginx-launch-config"
   image_id      = var.ami_id
